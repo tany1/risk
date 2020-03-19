e744c9629886b2e80e5812dfeda79da6b8f81ee9
Natanael Aitonean <natanael.aitonean@gmail.com>
2020-03-19 22:55:38 +0200

    input reading implementation

--- Main.java
+++ Main.java
@@ -1,12 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Country one = new Country(1);
-        Country two = new Country(2);
-        Country three = new Country(3);
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "input.txt";
+        Scanner scanner = new Scanner(new File(fileName));
+        scanner.useDelimiter("\n");
+        Integer line = 1;
+        Map map = new Map();
 
-        one.addNeighbor(two);
-        one.addNeighbor(three);
+        // Read neighbors
+        while (scanner.hasNext() && line <= 19) {
+            String lineText = scanner.next();
+            String[] splitted = lineText.split("\\s+");
 
-        
+            for (int neighborsCount = 1; neighborsCount < splitted.length; neighborsCount++) {
+                map.getCountries().get(line - 1).addNeighbor(map.getCountries().get(Integer.parseInt(splitted[neighborsCount]) - 1));
+            }
+            line++;
+        }
+
+        // Read the 20th line
+        String[] splitted = scanner.next().split("\\s+");
+        Integer answerPairs = Integer.parseInt(splitted[0].toString());
+        List<List<Integer>> pairs = new ArrayList<List<Integer>>(answerPairs);
+        line++;
+
+        // Read country pairs
+        while (scanner.hasNext() && answerPairs > 0) {
+            String lineText = scanner.next();
+            splitted = lineText.split("\\s+");
+            ArrayList<Integer> pair = new ArrayList<Integer>(2);
+
+            pair.add(Integer.parseInt(splitted[0]));
+            pair.add(Integer.parseInt(splitted[1]));
+            pairs.add(pair);
+
+            answerPairs--;
+            line++;
+        }
+
+        System.out.println(pairs);
+
+        scanner.close();
+
+        System.out.println(map);
     }
 }
